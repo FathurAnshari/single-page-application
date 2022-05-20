fca9fb9d19244a6317ee320fa5984d724bb6a9e6
FathurAnshari <anshariF17@gmail.com>
2022-05-20 17:08:19 +0700

    innit pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import AllQuote from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
-  return <div></div>;
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" exact>
+        <AllQuote />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
